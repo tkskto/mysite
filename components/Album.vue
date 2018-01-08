9741db78442dd7f5bead4e1818b23aae77f8378e
tkskto <tkskto@gmail.com>
2018-01-08 15:14:44 +0900

    change to components style

--- components/Album.vue
+++ components/Album.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="str-container">
-        <div class="container-canvas" ref="canvasWrap"></div>
+        <div id="canvasWrap" class="container-canvas"></div>
         <button class="btn-dot" :class="{'is-show': _viewFlg}" @click="backToAlbum">
             <svg class="svg-icon--dot" viewBox="0 0 20 9" xmlns="http://www.w3.org/2000/svg" title="一覧に戻る">
                 <circle r="1.5" fill="#fff" cx="5" cy="5"></circle>
@@ -11,47 +11,51 @@
     </div>
 </template>
 
-<script>
-    import {mapGetters} from 'vuex';
+<script lang="ts">
+    import Vue from 'vue';
+    import Component from 'nuxt-class-component';
+    import { Getter } from 'vuex-class';
     import * as THREE from 'three';
-    import TweenMax from 'gsap';
+    import gsap from 'gsap';
 
-    export default {
+
+    @Component({
         name: 'album',
         props: {
             _name: {
                 type: String,
                 required: true
             }
-        },
-        data () {
-            return {
-                _canvas: null,
-                _stage: null,
-                _renderer: null,
-                _mainCamera: null,
-                _container: null,
-                _geometry: null,
-                _material: null,
-                _rayCaster: null,
-                _intersects: [],
-                _mouseX: 0,
-                _mouseY: 0,
-                _viewFlg: false,
-                _selectedVideoID: 0,
-                _videos: {}
-            };
-        },
-        computed: {
-            ...mapGetters(['screenSize'])
-        },
-        created: function () {
+        }
+    })
+    export default class Album extends Vue {
+        @Getter screenSize;
+        _name = this._name;
+        _canvas;
+        _wrapper;
+        _stage;
+        _renderer;
+        _mainCamera;
+        _container;
+        _geometry;
+        _material;
+        _rayCaster;
+        _intersects = [];
+        _mouseX = 0;
+        _mouseY = 0;
+        _viewFlg = false;
+        _selectedVideoID = 0;
+        _videos = {};
+        _timer = 0;
+        _currentTween;
+
+        created () {
             this._stage = new THREE.Scene();
 
-            this._mainCamera = new THREE.PerspectiveCamera( 60, this.screenSize.width / this.screenSize.height, 1, 10000 );
-            this._mainCamera.position.set( 0, 0, 1000 );
+            this._mainCamera = new THREE.PerspectiveCamera(60, this.screenSize.width / this.screenSize.height, 1, 10000);
+            this._mainCamera.position.set(0, 0, 1000);
 
-            let ratio = window.devicePixelRatio;
+            const ratio = window.devicePixelRatio;
 
             this._renderer = new THREE.WebGLRenderer({
                 antialias: true,
@@ -66,14 +70,13 @@
 
             this._geometry = new THREE.SphereGeometry(100, 50, 50);
             this._geometry.scale(-1, 1, 1);
-
             this._material = new THREE.MeshBasicMaterial();
 
-            this._viewFlg = false;
             this._rayCaster = new THREE.Raycaster();
             this._videos = {};
-        },
-        mounted: function () {
+        }
+
+        mounted () {
             this._renderer.setSize(
                 this.screenSize.width, this.screenSize.height
             );
@@ -84,114 +87,125 @@
             this._renderer.clear();
             this._canvas = this._renderer.domElement;
 
-            this.$refs.canvasWrap.appendChild(this._canvas);
+            this._wrapper = document.getElementById('canvasWrap');
+
+            if (this._wrapper) {
+                this._wrapper.appendChild(this._canvas);
+            }
 
             this.addMovie('01');
 
             this.setEvent();
 
             this.play();
-        },
-        methods: {
-            setEvent: function () {
-                document.addEventListener('mousemove', this.onMouseMove);
-                document.addEventListener('click', this.viewDetail);
-            },
-            removeEvent: function () {
-                document.removeEventListener('mousemove', this.onMouseMove);
-                document.removeEventListener('click', this.viewDetail);
-            },
-            addPicture: function (_src) {
-                const geometry = this._geometry.clone();
-                const material = this._material.clone();
-            },
-            addMovie: function (_src) {
-                const video = document.createElement('video');
-                this._videos[_src] = video;
-                video.preload = 'none';
-                video.loop = true;
-                video.src = `/assets/album/${this._name}/${_src}.mp4`;
+        }
 
-                const geometry = this._geometry.clone();
-                const material = this._material.clone();
+        setEvent () {
+            document.addEventListener('mousemove', this.onMouseMove);
+            document.addEventListener('click', this.viewDetail);
+        }
 
-                const texture = new THREE.VideoTexture(video);
-                texture.minFilter = THREE.LinearFilter;
-                texture.magFilter = THREE.LinearFilter;
-                texture.format = THREE.RGBFormat;
-                material.map = texture;
+        removeEvent () {
+            document.removeEventListener('mousemove', this.onMouseMove);
+            document.removeEventListener('click', this.viewDetail);
+        }
 
-                const mesh = new THREE.Mesh(geometry, material);
-                mesh.scale.set(0.1, 0.1, 0.1);
-                mesh.userData.type = 'video';
-                mesh.userData.id = _src;
-                this._container.add(mesh);
+        addPicture (_src) {
+            const geometry = this._geometry.clone();
+            const material = this._material.clone();
+        }
 
-                video.addEventListener('loadeddata', () => {
-                    TweenMax.to(mesh.scale, 1.0, {
-                        x: 1.0,
-                        y: 1.0,
-                        z: 1.0,
-                        ease: TweenMax.Elastic.easeOut
-                    });
-                }, {
-                    once: true,
-                    passive: true,
-                    capture: false
+        addMovie (_src) {
+            const video = document.createElement('video');
+            this._videos[_src] = video;
+            video.preload = 'none';
+            video.loop = true;
+            video.src = `/assets/album/${this._name}/${_src}.mp4`;
+
+            const geometry = this._geometry.clone();
+            const material = this._material.clone();
+
+            const texture = new THREE.VideoTexture(video);
+            texture.minFilter = THREE.LinearFilter;
+            texture.magFilter = THREE.LinearFilter;
+            texture.format = THREE.RGBFormat;
+            material.map = texture;
+
+            const mesh = new THREE.Mesh(geometry, material);
+            mesh.scale.set(0.1, 0.1, 0.1);
+            mesh.userData.type = 'video';
+            mesh.userData.id = _src;
+            this._container.add(mesh);
+
+            video.addEventListener('loadeddata', () => {
+                this._currentTween = new gsap.TweenMax(mesh.scale, 1.0, {
+                    x: 1.0,
+                    y: 1.0,
+                    z: 1.0,
+                    ease: gsap.Elastic.easeOut
                 });
+            }, {
+                once: true,
+                passive: true,
+                capture: false
+            });
 
-                video.load();
-            },
-            onMouseMove: function () {
-                this._mouseX = ( event.clientX / this.screenSize.width ) * 2 - 1;
-                this._mouseY = - ( event.clientY / this.screenSize.height ) * 2 + 1;
-                this._rayCaster.setFromCamera( {x: this._mouseX, y: this._mouseY}, this._mainCamera );
-                this._intersects = this._rayCaster.intersectObjects( this._container.children );
-            },
-            viewDetail: function () {
-                if (this._intersects.length > 0) {
-                    this.removeEvent();
+            video.load();
+        }
 
-                    const mesh = this._container.children[0];
-                    this._selectedVideoID = mesh.userData.id;
+        onMouseMove (event) {
+            this._mouseX = (event.clientX / this.screenSize.width) * 2 - 1;
+            this._mouseY = - (event.clientY / this.screenSize.height) * 2 + 1;
+            this._rayCaster.setFromCamera({x: this._mouseX, y: this._mouseY}, this._mainCamera);
+            this._intersects = this._rayCaster.intersectObjects(this._container.children);
+        }
 
-                    TweenMax.to(this._mainCamera.position, 1.0, {
-                        z: mesh.position.z,
-                        ease: TweenMax.Power3.easeOut,
-                        onComplete: () => {
-                            this._viewFlg = true;
-                            console.log(this._viewFlg);
-                            if (mesh.userData.type === 'video') {
-                                this._videos[this._selectedVideoID].play();
-                            }
-                        }
-                    });
-                }
-            },
-            backToAlbum: function () {
-                this._videos[this._selectedVideoID].pause();
-                TweenMax.to(this._mainCamera.position, 1.0, {
-                    z: 1000,
-                    ease: TweenMax.Power3.easeIn,
+        viewDetail () {
+            if (this._intersects.length > 0) {
+                this.removeEvent();
+
+                const mesh = this._container.children[0];
+                this._selectedVideoID = mesh.userData.id;
+
+                this._currentTween = new gsap.TweenMax(this._mainCamera.position, 1.0, {
+                    z: mesh.position.z,
+                    ease: gsap.Power3.easeOut,
                     onComplete: () => {
-                        this._viewFlg = false;
-                        this.setEvent();
+                        this._viewFlg = true;
+                        if (mesh.userData.type === 'video') {
+                            this._videos[this._selectedVideoID].play();
+                        }
                     }
                 });
-            },
-            play: function () {
-                this.update();
-            },
-            pause: function () {
-                if (this._timer) {
-                    cancelAnimationFrame(this._timer);
-                    this._timer = null;
-                }
-            },
-            update: function () {
-                this._timer = requestAnimationFrame(this.update);
-                this._renderer.render(this._stage, this._mainCamera);
             }
+        }
+
+        backToAlbum () {
+            this._videos[this._selectedVideoID].pause();
+            this._currentTween = new gsap.TweenMax(this._mainCamera.position, 1.0, {
+                z: 1000,
+                ease: gsap.Power3.easeIn,
+                onComplete: () => {
+                    this._viewFlg = false;
+                    this.setEvent();
+                }
+            });
+        }
+
+        play () {
+            this.update();
+        }
+
+        pause () {
+            if (this._timer) {
+                cancelAnimationFrame(this._timer);
+                this._timer = -1;
+            }
+        }
+
+        update () {
+            this._timer = requestAnimationFrame(this.update);
+            this._renderer.render(this._stage, this._mainCamera);
         }
     }
 </script>

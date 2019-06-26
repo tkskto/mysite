1479b4e3e190697b53ff26b264faeba1bdc752e7
kato takeshi <tkskto@gmail.com>
2019-06-27 01:13:19 +0900

    update package

--- modules/typescript.ts
+++ modules/typescript.ts
@@ -1,51 +1,21 @@
-module.exports = function (options) {
-    // Add .ts extension for store, middleware and more
+module.exports = function () {
+    // Add .ts & .tsx extension to Nuxt
     this.nuxt.options.extensions.push('ts');
+
     // Extend build
-    this.extendBuild((config) => {
+    this.extendBuild(config => {
+        // Add TypeScript
         config.module.rules.push({
             test: /\.ts$/,
-            loader: 'tslint-loader',
-            enforce: 'pre',
-            options: {
-                configFile: 'tslint.json',
-                tsConfigFile: 'tsconfig.json',
-            },
-        });
-        const tsLoader = {
             loader: 'ts-loader',
             options: {
                 appendTsSuffixTo: [/\.vue$/],
-            },
-        };
-        // Add TypeScript loader
-        // config.module.rules.push(Object.assign(
-        //     {
-        //         test: /((client|server)\.js)|(\.tsx?)$/,
-        //     },
-        //     tsLoader,
-        // ));
-        // Add TypeScript loader for vue files
-        config.module.rules.push({
-            test: /\.ts$/,
-            loader: 'ts-loader',
-            options: {
                 configFile: 'tsconfig.json',
             },
         });
-        for (const rule of config.module.rules) {
-            if (rule.loader === 'vue-loader') {
-                rule.options.loaders.ts = tsLoader;
-            }
-        }
-        // for (const rule of config.module.rules) {
-        //     if (rule.loader === 'vue-loader') {
-        //         rule.options.loaders.ts = 'ts-loader?{"appendTsSuffixTo":["\\\\.vue$"], "configFile": "tsconfig.json"}!tslint-loader';
-        //     }
-        // }
         // Add .ts extension in webpack resolve
-        if (config.resolve.extensions.indexOf('.ts') === -1) {
-            config.resolve.extensions.push('.ts');
+        if (!config.resolve.extensions.includes('.ts')) {
+            config.resolve.extensions.push('.ts')
         }
-    });
+    })
 };
